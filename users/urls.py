f47896ecfb99a982040b7a5d2e986fc65a81c7eb
Kazmi G <104306072+KazmiG0099@users.noreply.github.com>
2023-08-09 14:00:12 +0500

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    # path('auth/users/', CustomRegistrationView.as_view(), name='registration'),
 ]
--- users/views.py
+++
@@ -1,5 +0,0 @@
-# from django.shortcuts import render
-# from djoser.views import UserCreateView
-
-# class CustomUserRegistrationView(UserCreateView):
-#     template_name = "custom_registration.html"
